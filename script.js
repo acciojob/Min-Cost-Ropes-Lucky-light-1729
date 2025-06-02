217468528761d783cf7c2e94e1ca5a92a17c268f
Lucky-light-1729 <agrawalanay20@gmail.com>
2025-06-02 09:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+  let totalCost = 0;
+
+  while (arr.length > 1) {
+    // Sort array to get the two smallest ropes at the front
+    arr.sort((a, b) => a - b);
+
+    // Take the two smallest ropes
+    const first = arr.shift();
+    const second = arr.shift();
+
+    const cost = first + second;
+    totalCost += cost;
+    arr.push(cost);
+  }
+
+  return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
